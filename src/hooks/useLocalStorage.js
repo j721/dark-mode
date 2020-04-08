cf1b944dad0ba8298b2ace33038922ce8f351f50
j721 <juliadvu@gmail.com>
2020-04-08 16:45:10 -0500

    return setValue or setStoredValue still works for toggle switch dark mode

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -12,7 +12,7 @@ function useLocalStorage (key, initialValue) {
         window.localStorage.setItem(key, JSON.stringify(value));
     };
 
-    return [storedValue, setStoredValue];
+    return [storedValue, setValue];
 }
 
 export default useLocalStorage; 
