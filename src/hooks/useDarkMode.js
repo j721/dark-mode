c19223058ecbe63a750100086b19588fea2b19aa
j721 <juliadvu@gmail.com>
2020-04-08 15:46:34 -0500

    useDarkMode hook changes made

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -2,13 +2,26 @@ import useLocalStorage from "./useLocalStorage";
 import {useEffect} from "react";
 
 const useDarkMode =(key, initialValue) =>{
-//Call useLocalStorage  and pass in key
+//Call useLocalStorage  and pass in key and initial value
     const [darkMode, setDarkMode] = useLocalStorage(key, initialValue);
     
     const toggleDarkMode= event =>{
         event.preventDefault();
+        //switch between DarkMode state true or false
         return darkMode ? setDarkMode(false): setDarkMode(true);
-    }
+    };
 
-    return [toggleDarkMode];
-}
+    //useEffect to add or remove dark-mode class and DOM maniuplation
+    useEffect(()=>{
+        if (darkMode){
+            document.body.classList.add("dark-mode")
+        }
+        else{
+            document.body.classList.remove("dark-mode")
+        }
+    },[darkMode]);
+
+    return [toggleDarkMode, darkMode, setDarkMode];
+};
+
+export default useDarkMode; 
